77c77a8651291c236fe57c0c22d4c26c28258fdc
renzomontenegro <renzomogra@gmail.com>
2026-08-09 01:11:46 -0500

    Bump version to force redeploy with VAPID env var (v3.6)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import type { Macros, MealSlot } from '../types'
 
-export const APP_VERSION = '3.5'
+export const APP_VERSION = '3.6'
 
 // ---------------------------------------------------------------------------
 // Valores iniciales
