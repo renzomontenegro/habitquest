38223994790918fbe68243124e2c89ddfccd4f0d
renzomontenegro <renzomogra@gmail.com>
2026-08-09 01:24:18 -0500

    Bump version to redeploy with corrected VAPID key (v3.9)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import type { Macros, MealSlot } from '../types'
 
-export const APP_VERSION = '3.8'
+export const APP_VERSION = '3.9'
 
 // ---------------------------------------------------------------------------
 // Valores iniciales
