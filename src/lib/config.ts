64f4d2fecf56ecd5abbf3d4c6e0e36b247f6f891
renzomontenegro <renzomogra@gmail.com>
2026-09-12 22:43:32 -0500

    Fix meal selector overflow on mobile

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import type { Macros, MealSlot } from '../types'
 
-export const APP_VERSION = '3.54'
+export const APP_VERSION = '3.55'
 
 // ---------------------------------------------------------------------------
 // Valores iniciales
