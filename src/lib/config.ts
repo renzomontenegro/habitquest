ce748b54088a98481b34a70fa5d914123acf350d
renzomontenegro <renzomogra@gmail.com>
2026-09-13 16:17:35 -0500

    style: refresh app with athletic visual system

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import type { Macros, MealSlot } from '../types'
 
-export const APP_VERSION = '3.68'
+export const APP_VERSION = '3.70'
 
 // ---------------------------------------------------------------------------
 // Valores iniciales
