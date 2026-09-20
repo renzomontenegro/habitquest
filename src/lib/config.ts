08f3107f5eab077d66be8bb6a2f13b0a785418f7
renzomontenegro <renzomogra@gmail.com>
2026-09-20 17:47:10 -0500

    fix: guarantee bottom margin at sheet scroll end

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import type { Macros, MealSlot } from '../types'
 
-export const APP_VERSION = '3.95'
+export const APP_VERSION = '3.96'
 
 // ---------------------------------------------------------------------------
 // Valores iniciales
